54bf81e01f6da9dc586ea82a53120386186bdd95
PranavRasal <pranavcode19@gmail.com>
2025-05-28 00:12:59 +0530

    Implement password generation logic and update HTML background color

--- password/src/App.jsx
+++ password/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
+  const  [lenght, setLenght] = useState(8);
+  const [numberAllow ,setNumberAllow] = useState(false);
+  const [charAllow,setCharAllow] = useState(false);
+  const passwordGenerator = useCallback(()=>{
+   let pass ="";
+   let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+   if (numberAllow) str += "0123456789";
+   if (charAllow) str += "!@#$%^&*()_+=?/|;:{}[]";
+   
+   for(i=1;  i<=lenght; i++){
+    pass=str.charAt[maths.floor(maths.rondom() * str.length + 1 )]
+   }
+    setPassword(pass)
+  }
+    ,[lenght ,numberAllow,charAllow])
 
+  
   return (
     <>
-
+   <h1>hw=eel</h1>
     </>
   )
 }
